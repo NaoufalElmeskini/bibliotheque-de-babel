a10723bf52de0c3a7683b606c4e310ed76dcaccb
Naoufal EL MESKINI <naoufal_el_meskini@carrefour.com>
2023-10-07 09:26:54 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapperDiv">
+    <div class="block">
+      <ControlCenter></ControlCenter>
+    </div>
+    <div class="block">
+      <Dashboard></Dashboard>
+    </div>
+  </div>
+</template>
+
+<script>
+import Dashboard from "@/components/DashboardView.vue";
+import ControlCenter from "@/components/ControlCenter.vue";
+export default {
+  name: 'App',
+  components: {
+    ControlCenter,
+    Dashboard,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.wrapperDiv {
+  width: 100%;
+  height: 500px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border-radius: 4%;
+}
+
+.block {
+  width: 45%;
+  height: 500px;
+
+  border: solid #42b983;
+  border-width: thick;
+  border-radius: 8%;
+
+}
+
+</style>
